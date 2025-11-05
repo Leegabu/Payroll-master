45801949ff9f4f2f081f94099795ac25e930c366
Leegabu <leeirungu11313@gmail.com>
2025-11-05 20:10:05 +0300

    Added composer files and dependencies

--- backend/config/MailConfig.php
+++ backend/config/MailConfig.php
@@ -4,8 +4,8 @@ namespace Backend\Config;
 class MailConfig
 {
     public const SMTP_HOST = 'smtp.gmail.com';
-    public const SMTP_USER = 'yourgmail@gmail.com'; // your Gmail address
-    public const SMTP_PASS = 'abcd efgh ijkl mnop'; // your Gmail App Password (NOT your login password)
+    public const SMTP_USER = 'yourgmail@gmail.com'; 
+    public const SMTP_PASS = 'abcd efgh ijkl mnop'; 
     public const SMTP_PORT = 465;
     public const SMTP_SECURE = 'ssl';
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0f1b89ede8d6a6ed1d3640372ea65c0d0d4759f7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0f1b89ede8d6a6ed1d3640372ea65c0d0d4759f7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => 'c7111310c6116ba508a6a170a89eaaed2129bd42',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
